bd8d780e53b9a1a9844db5d0d1da76574969323f
Nurkholiq <mnurkholiq114@gmail.com>
2022-06-21 05:08:38 +0700

    first commit

--- src/config/API.js
+++ src/config/API.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const API = axios.create({
   baseURL:
-    // process.env.REACT_APP_SERVER_URL ||
-    // "https://waysbeans-app-backend.herokuapp.com/api/v1" ||
+    process.env.SERVER_URL ||
+    "https://waysbeans-app-backend.herokuapp.com/api/v1" ||
     "http://localhost:5000/api/v1",
 });
 
